7a29f6181af32e01d557883b6d5963ac6c663fad
Artem Zidkov <artemzidkov385@gmail.com>
2023-10-05 00:10:28 +0300

    Написал реализацию функции-туториала

--- Tic-tac-toe/Game.cpp
+++ Tic-tac-toe/Game.cpp
@@ -8,7 +8,28 @@ char table[3][3];
 bool step;
 
 
-void instruction();
+void instruction()
+{
+	cout << "***** Tic-tac-toe game ***** \n\n";
+	cout << "Rules:\n";
+	cout << "2 players play on a 3v3 field\n";
+	cout << "The one who makes the winning combination wins\n";
+	cout << "Example field:\n\n";
+
+	int l = 0;
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << "| " << l + 1 << " ";
+			table[i][j] = char(49 + l++);
+		}
+		cout << "|";
+		cout << endl;
+	}
+	_getch();
+}
 
 int main() {
 
