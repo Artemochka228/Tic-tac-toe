adaca89b2a4c6f20b7035fbf8265224828246724
Artem Zidkov <artemzidkov385@gmail.com>
2023-10-07 13:25:43 +0300

    Изменил функцию определения победителя, чтобы она могла определять ничью

--- Tic-tac-toe/Game.cpp
+++ Tic-tac-toe/Game.cpp
@@ -105,6 +105,17 @@ bool checkWinner()
 			return true;
 		}
 	}
+
+	int cnt = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			if (table[i][j] != 'X' && table[i][j] != 'O') break;
+			cnt++;
+		}
+	}
+	if (cnt == 9) return true;
 	return false;
 }
 
